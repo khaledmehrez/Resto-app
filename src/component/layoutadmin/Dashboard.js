7afa82e182cb03538de15433c3eb09370a366afb
khaled mehrez <khaledmehrezsimplon@gmail.com>
2020-06-16 01:53:10 +0100

    edit admindashboard

--- src/component/layoutadmin/Dashboard.js
+++ src/component/layoutadmin/Dashboard.js
@@ -3,13 +3,23 @@ import { connect } from "react-redux";
 import { getOrderAPi } from "../../api/api";
 import Order from "../layout client/Order";
 import Modalorder from "./Modalorder";
+
 class Dashboard extends Component {
-  state={validation:false}
+  state={validation1:false,validation2:false,validation3:false,validation4:false,validation5:false}
   componentDidMount() {
     this.props.getOrder();
   }
-  validate=()=>{
-    this.setState({validation:true})
+  validate1=()=>{
+    this.setState({validation1:true})
+  }
+  validate2=()=>{
+    this.setState({validation2:true})
+  }
+  validate3=()=>{
+    this.setState({validation3:true})
+  }
+  validate4=()=>{
+    this.setState({validation4:true})
   }
   render() {
     return (
@@ -25,14 +35,50 @@ class Dashboard extends Component {
           <tbody class="">
             <tr class="">
               <td class="">
-                <div class="ui ribbon label">user</div>
+                <div class="ui ribbon label">user1</div>
               </td>
               <td class="">
-                <Modalorder />
+                <Modalorder getorders={this.props.getorders} b={"user1"}/>
               </td>
               <td class="">
                 {" "}
-                {(this.state.validation===false)?<button class="ui red basic button" onClick={this.validate}>Not validate</button>: <button class="ui green basic button">validate</button>}
+                {(this.state.validation1===false)?<button class="ui red basic button" onClick={this.validate1}>Not validate</button>: <button class="ui green basic button">validate</button>}
+              </td>
+            </tr>
+            <tr class="">
+              <td class="">
+                <div class="ui ribbon label">user2</div>
+              </td>
+              <td class="">
+                <Modalorder getorders={this.props.getorders} b={"user2"}/>
+              </td>
+              <td class="">
+                {" "}
+                {(this.state.validation2===false)?<button class="ui red basic button" onClick={this.validate2}>Not validate</button>: <button class="ui green basic button">validate</button>}
+              </td>
+            </tr>
+            <tr class="">
+              <td class="">
+                <div class="ui ribbon label">user3</div>
+              </td>
+              <td class="">
+                <Modalorder getorders={this.props.getorders} b={"user3"}/>
+              </td>
+              <td class="">
+                {" "}
+                {(this.state.validation3===false)?<button class="ui red basic button" onClick={this.validate3}>Not validate</button>: <button class="ui green basic button">validate</button>}
+              </td>
+            </tr>
+            <tr class="">
+              <td class="">
+                <div class="ui ribbon label">user4</div>
+              </td>
+              <td class="">
+                <Modalorder getorders={this.props.getorders} b={"user4"}/>
+              </td>
+              <td class="">
+                {" "}
+                {(this.state.validation4===false)?<button class="ui red basic button" onClick={this.validate4}>Not validate</button>: <button class="ui green basic button">validate</button>}
               </td>
             </tr>
           </tbody>
--- src/component/layoutadmin/Modalorder.js
+++ src/component/layoutadmin/Modalorder.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 import Order from '../layout client/Order'
-
-const Modalorder = () => (
+import Ordercard from '../layout client/Ordercard';
+import "./dashboard.css"
+const Modalorder = (props) => (
+  
   <Modal trigger={<Button>Show Modal</Button>}>
     <Modal.Header>Select a Photo</Modal.Header>
     <Modal.Content image>
-      
+    
       <Modal.Description>
         <Header>Default Profile Image</Header>
-        <Order />
+        <div class="dashboard-container">
+        {props.getorders.filter(el=>el[props.b]===props.b).map(el=><Ordercard  orderfood={el} b={props.b} />)}
+        </div>
       </Modal.Description>
     </Modal.Content>
   </Modal>
